7093160d681bba7b5d0bd5032e039d8c8492edab
ssmitee <144239782+ssmitee@users.noreply.github.com>
2024-03-02 12:14:55 +0200

    Добавьте файлы проекта.

---
+++ 3.2 visokiy fom/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Fibonacci
+{
+    static void Main()
+    {
+        const int position = 20;
+        long fib0 = 0;
+        long fib1 = 1;
+        long fibN = 0;
+
+        for (int i = 2; i <= position; i++)
+        {
+            fibN = fib0 + fib1;
+            fib0 = fib1;
+            fib1 = fibN;
+        }
+
+        Console.WriteLine($"20-е число Фибоначчи: {fibN}");
+    }
+}
